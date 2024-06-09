d7f3915cb84bca2cc3bd599bb48ac89508df2790
crash1604 <cs.cucse@gmail.com>
2024-06-08 23:25:55 -0400

    imports changed

--- backend/convert/api/services.py
+++ backend/convert/api/services.py
@@ -1,24 +1,34 @@
 import requests
+import logging
 from .models import Currency
+
 
 API_URL = "https://api.frankfurter.app/"
 
+logger = logging.getLogger(__name__)
+ 
+
 def get_or_create_currency():
-    currencies = Currency.objects.all()
+    try:
+        currencies = Currency.objects.all()
 
-    if currencies.exists():
-        return currencies
+        if currencies.exists():
+            return currencies
 
-    temp = API_URL + 'currencies'
-    response = requests.get(temp)
-    response.raise_for_status()  # Raise an exception for HTTP errors
+        response = requests.get(API_URL)
+        response.raise_for_status()  # Raise an exception for HTTP errors
 
-    data = response.json()
-    currency_objects = [
-        Currency(code=code, name=name) for code, name in data.items()
-    ]
+        data = response.json()
+        currency_objects = [
+            Currency(code=code, name=name) for code, name in data.items()
+        ]
 
-    Currency.objects.bulk_create(currency_objects)
+        Currency.objects.bulk_create(currency_objects)
 
-    return Currency.objects.all()
-
+        return Currency.objects.all()
+    except requests.RequestException as e:
+        logger.error(f"Error fetching data from API: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+        raise
