ab6f3780abe3c410cad5e6828396fb6ea52a9266
crash1604 <cs.cucse@gmail.com>
2024-06-10 18:03:46 -0400

    removed dashboard component and added header

--- frontend/currency-exchange/src/App.js
+++ frontend/currency-exchange/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
+// src/App.jsx
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Convert from './components/Convert';
+import History from './components/History';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 
 function App() {
+
   return (
-    <div className="App">
-     <Dashboard />
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <Header />
     </div>
   );
 }
